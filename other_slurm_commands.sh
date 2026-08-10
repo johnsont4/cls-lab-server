3c9786523cfe79f9a8c57f53678c49687ccc25f7
johnsont4 <teagan.johnson@colorado.edu>
2026-08-10 11:35:18 -0600

    add top command

--- other_slurm_commands.sh
+++ other_slurm_commands.sh
@@ -5,3 +5,11 @@ tree -ah --du -L 2 # looks at hidden directories too
 
 du -sh ~ # total size of home directory
 du -ah --max-depth=1 ~ | sort -h # sort by size, largest last
+
+# monitor system
+# nice to get PID (id used for a kill command) and general mem usage. USER is who owns it
+# this is most helpful on compute nodes (login nodes don't show much useful info)
+# for gpu usage, see the gpu slurm commands file
+top
+    # inside top, you can press 'M' to sort by memory usage, 'P' to sort by CPU usage, and 'u' to filter by a specific user
+top -u tejo9855 # user-specific
